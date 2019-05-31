246ca48ce333a11960c3e57c59028a24c1591fba
kzmaro <maronkazu94@gmail.com>
2019-05-31 23:57:07 +0900

    #2 積の実装

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def add(a:int,b:int) -> int:
     return a+b
-
+def seki(a:int,b:int) -> int:
+    return a*b
 
 if __name__=='__main__':
-    print(f'2+3={add(2,3)}')
+    print(f'2*3={seki(2,3)}')
