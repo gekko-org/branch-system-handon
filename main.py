7af3509683f4db12d49885090b197741faaf075f
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-05-31 23:56:32 +0900

    #1 実装

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def add(a:int,b:int) -> int:
-    return a+b
+def add(a: int, b: int) -> int:
+    return a + b
 
 
-if __name__=='__main__':
-    print(f'2+3={add(2,3)}')
+def sub(a: int, b: int) -> int:
+    return a - b
+
+
+if __name__ == '__main__':
+    print(f'2+3={add(2, 3)}')
+    print(f'2-3={sub(2, 3)}')
